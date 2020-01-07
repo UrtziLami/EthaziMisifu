cf9b5d714f803a7c80d1135cb7932dd29dad1cb3
AitroNogales <aitor.nogalesor@elorrita-errekamari.com>
2020-01-07 22:42:24 +0100

    configuracion hibernate (error)

--- AccesoaDatos/src/main/java/Ethazi/AccesoaDatos/App.java
+++ AccesoaDatos/src/main/java/Ethazi/AccesoaDatos/App.java
@@ -1,5 +1,9 @@
 package Ethazi.AccesoaDatos;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       SessionFactory sf = new Configuration().configure().buildSessionFactory();
+       Session session =sf.openSession();
+       session.beginTransaction();
+       System.out.println("transaction beggin");
     }
 }
